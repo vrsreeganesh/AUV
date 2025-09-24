90219b5c44e366a512939cfa145c1075dd6fd7ad
vrsreeganesh <vrsreeganesh@gmail.com>
2025-09-24 18:44:50 -0400

    Added: csv-file write operations, concatenate, linspace, max, meshgrid, min

---
+++ Code/C++/include/svr_meshgrid.hpp
@@ -0,0 +1,20 @@
+template <typename T>
+auto meshgrid(std::vector<T>    x,
+              std::vector<T>    y)
+{
+
+    // creating and filling x-grid
+    std::vector<std::vector<T>> xcanvas(y.size(), std::vector<T>(x.size(), 0));
+    for(auto row = 0; row < y.size(); ++row)  
+        std::copy(x.begin(), x.end(), xcanvas[row].begin());
+
+    // creating and filling y-grid
+    std::vector<std::vector<T>> ycanvas(y.size(), std::vector<T>(x.size(), 0));
+    for(auto col = 0; col < x.size(); ++col)
+        for(auto row = 0; row < y.size(); ++row)
+            ycanvas[row][col]   = y[row];
+
+    // returning
+    return std::pair{xcanvas, ycanvas};
+
+}
